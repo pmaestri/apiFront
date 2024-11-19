6375c3e69860c60d065bd1f2b4b75a0a64b2b70a
Paula Maestri <pmaestri@uade.edu.ar>
2024-11-18 22:01:43 -0300

    actualizacion store

--- src/api/store.jsx
+++ src/api/store.jsx
@@ -3,11 +3,13 @@ import authReducer from './AuthSlice'
 import usuariosReducer from './UserSlice';
 import categoriasReducer from './CategorySlice';
 import pedidosReducer from './OrderSlice';
+import productoReducer from './ProductSlice';
 export const store = configureStore({
     reducer:{
         auth: authReducer,
         usuarios: usuariosReducer,
         categorias: categoriasReducer,
         pedidos: pedidosReducer,
+        producto: productoReducer,
     },
 });
